0fe6c4969ee99ae9f03d9587006328f100cd3cbc
Johe <jiayihsu0518@gmail.com>
2023-11-25 00:31:48 +0800

    strong finish

---
+++ hw04/src-strong/77df75ddaaf4585d@25.js
@@ -0,0 +1,210 @@
+function _1(md){return(
+md`# HW04 Strong`
+)}
+
+function _artist(FileAttachment){return(
+FileAttachment("artist-1.csv").csv()
+)}
+
+function _innerCircleQuestion(artist){return(
+Object.keys(artist[0])[1]
+)}
+
+function _outerCircleQuestion(artist){return(
+Object.keys(artist[0])[16]
+)}
+
+function _data(artist,innerCircleQuestion,outerCircleQuestion,buildHierarchy)
+{
+  // 提取內外圈問題的答案
+  var innerCircleAnswer = artist.map(row => row[innerCircleQuestion]);
+  var outerCircleAnswer = artist.map(row => row[outerCircleQuestion]);
+
+  // 將內外圈答案結合，形成新的答案陣列
+  var combinedAnswers = innerCircleAnswer.map((innerAns, index) => innerAns + '-' + outerCircleAnswer[index]);
+
+  // 重新格式化答案，將其轉換為符合特定模式的陣列
+  var reformattedAnswers = combinedAnswers.map(item => {
+    const [prefix, values] = item.split('-');
+    const splitValues = values.split(';').map(value => value.trim());
+    return splitValues.map(value => `${prefix}-${value}`);
+  }).reduce((acc, curr) => acc.concat(curr), []);
+
+  // 計算每個重新格式化答案的出現次數
+  var answerCounts = {};
+  reformattedAnswers.forEach(reformattedAns => {
+    answerCounts[reformattedAns] = (answerCounts[reformattedAns] || 0) + 1;
+  });
+
+  // 轉換為CSV格式的數據
+  var csvData = Object.entries(answerCounts).map(([answer, count]) => [answer, String(count)]);
+  
+  // 建立包含層次結構的數據
+  return buildHierarchy(csvData);
+}
+
+
+function _chart(d3,data)
+{
+
+  // Specify the chart’s dimensions.
+  const width = 800;
+  const height = width;
+
+  // Create the color scale.
+  const color = d3.scaleLinear()
+      .domain([0, 5])
+      .range(["hsl(204,25%,90%)", "hsl(215,100%,35%)"])
+      .interpolate(d3.interpolateHcl);
+
+  // Compute the layout.
+  const pack = data => d3.pack()
+      .size([width, height])
+      .padding(3)
+    (d3.hierarchy(data)
+      .sum(d => d.value)
+      .sort((a, b) => b.value - a.value));
+  const root = pack(data);
+
+  // Create the SVG container.
+  const svg = d3.create("svg")
+      .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+      .attr("width", width)
+      .attr("height", height)
+      .attr("style", `max-width: 100%; height: auto; display: block; margin: 0 -14px; background: ${color(0)}; cursor: pointer;`);
+
+  // Append the nodes.
+  const node = svg.append("g")
+    .selectAll("circle")
+    .data(root.descendants().slice(1))
+    .join("circle")
+      .attr("fill", d => d.children ? color(d.depth) : "white")
+      .attr("pointer-events", d => !d.children ? "none" : null)
+      .on("mouseover", function() { d3.select(this).attr("stroke", "#A58E80"); })
+      .on("mouseout", function() { d3.select(this).attr("stroke", null); })
+      .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()));
+
+  // Append the text labels.
+  const label = svg.append("g")
+      .style("font", "12px sans-serif")
+      .attr("pointer-events", "none")
+      .attr("text-anchor", "middle")
+    .selectAll("text")
+    .data(root.descendants())
+    .join("text")
+      .style("fill-opacity", d => d.parent === root ? 1 : 0)
+      .style("display", d => d.parent === root ? "inline" : "none")
+      .text(d => d.data.name);
+
+  // Create the zoom behavior and zoom immediately in to the initial focus node.
+  svg.on("click", (event) => zoom(event, root));
+  let focus = root;
+  let view;
+  zoomTo([focus.x, focus.y, focus.r * 2]);
+
+  function zoomTo(v) {
+    const k = width / v[2];
+
+    view = v;
+
+    label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+    node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+    node.attr("r", d => d.r * k);
+  }
+
+  function zoom(event, d) {
+    const focus0 = focus;
+
+    focus = d;
+
+    const transition = svg.transition()
+        .duration(event.altKey ? 7500 : 750)
+        .tween("zoom", d => {
+          const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+          return t => zoomTo(i(t));
+        });
+
+    label
+      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+      .transition(transition)
+        .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+        .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+        .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+  }
+
+  return svg.node();
+}
+
+
+function _7(md){return(
+md`<h2>結論</h2>
+<h3>從上圖中，我們可以看出：
+  <ul>
+    <li>這個問卷調查對象太偏頗，工作室、替代空間以及非營利組織/藝術團體三者就佔多數，因而調查的結果代表性可能不足。</li>
+    <br>
+    <li>幾乎所有的單位選擇的前兩大答案都是減少包裝以及再生材料，顯示這兩種做法是最容易達成的。</li>
+  </ul>
+</h3>`
+)}
+
+function _buildHierarchy(){return(
+function buildHierarchy(csv) {
+  // Helper function that transforms the given CSV into a hierarchical format.
+  const root = { name: "root", children: [] };
+  for (let i = 0; i < csv.length; i++) {
+    const sequence = csv[i][0];
+    const size = +csv[i][1];
+    if (isNaN(size)) {
+      // e.g. if this is a header row
+      continue;
+    }
+    const parts = sequence.split("-");
+    let currentNode = root;
+    for (let j = 0; j < parts.length; j++) {
+      const children = currentNode["children"];
+      const nodeName = parts[j];
+      let childNode = null;
+      if (j + 1 < parts.length) {
+        // Not yet at the end of the sequence; move down the tree.
+        let foundChild = false;
+        for (let k = 0; k < children.length; k++) {
+          if (children[k]["name"] == nodeName) {
+            childNode = children[k];
+            foundChild = true;
+            break;
+          }
+        }
+        // If we don't already have a child node for this branch, create it.
+        if (!foundChild) {
+          childNode = { name: nodeName, children: [] };
+          children.push(childNode);
+        }
+        currentNode = childNode;
+      } else {
+        // Reached the end of the sequence; create a leaf node.
+        childNode = { name: nodeName, value: size };
+        children.push(childNode);
+      }
+    }
+  }
+  return root;
+}
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  function toString() { return this.url; }
+  const fileAttachments = new Map([
+    ["artist-1.csv", {url: new URL("./files/50c5f49fe0be94803e1d055aaca895517461971b40fc91cc553044dee88321c73843858f823684904e961847c7cc5d3bb60f8ecd7cdfde293dd65a61d08e1e2d.csv", import.meta.url), mimeType: "text/csv", toString}]
+  ]);
+  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer("artist")).define("artist", ["FileAttachment"], _artist);
+  main.variable(observer("innerCircleQuestion")).define("innerCircleQuestion", ["artist"], _innerCircleQuestion);
+  main.variable(observer("outerCircleQuestion")).define("outerCircleQuestion", ["artist"], _outerCircleQuestion);
+  main.variable(observer("data")).define("data", ["artist","innerCircleQuestion","outerCircleQuestion","buildHierarchy"], _data);
+  main.variable(observer("chart")).define("chart", ["d3","data"], _chart);
+  main.variable(observer()).define(["md"], _7);
+  main.variable(observer("buildHierarchy")).define("buildHierarchy", _buildHierarchy);
+  return main;
+}
---
+++ hw04/src-strong/index.js
@@ -0,0 +1,1 @@
+export {default} from "./77df75ddaaf4585d@25.js";
